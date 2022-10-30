9e90ee0726c5cf5d64ee95da04a68aab5e9fde23
chriscodes01 <chriscodes.business@gmail.com>
2022-10-30 17:01:36 -0500

    Add files via upload

---
+++ movies/13.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT(name) FROM people
+JOIN stars ON stars.person_id = people.id
+JOIN movies ON movies.id = stars.movie_id
+WHERE name != "Kevin Bacon" AND movies.id IN (SELECT movies.id FROM movies
+JOIN stars ON stars.movie_id = movies.id
+JOIN people ON people.id = stars.person_id
+WHERE people.name = "Kevin Bacon" AND people.birth = 1958);
